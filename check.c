71af93b462c3af72be0520f97ec5df2d1c14db62
Chen-Yuanmeng <2784841400@qq.com>
2024-11-09 15:32:06 +0800

    Change `prohibit` to `forbidden`; modify return rules of `check` functions

--- check.c
+++ check.c
@@ -1,29 +1,39 @@
 #include "check.h"
 
 // Check entrance function: start check of all things
-// return `int`: 0b000 - passed check; others - failed check
+// return `int`:
+//     0000 - passed check;
+//     others - failed check
 int check(int board_state[][BOARD_SIZE], int last_put[]) {
-    return (
-        check_win(board_state, last_put) + 
-        check_prohibit_move(board_state, last_put) + 
-        check_full_board(board_state, last_put)
-    );
+    // return (
+    //     check_win(board_state, last_put) + 
+    //     check_forbidden_move(board_state, last_put) + 
+    //     check_full_board(board_state, last_put)
+    // );
+    return 0;
 }
 
 // Check win function: check if anyone wins
-// return `int`: 0b0 - passed check; 0b1 - failed check
+// return `int`:
+//     00 - Nobody wins;
+//     01 - Black wins;
+//     02 - White wins;
 int check_win(int board_state[][BOARD_SIZE], int last_put[]) {
 
 }
 
-// Check prohibit function: check if Black has performed prohibited moves
-// return `int`: 0b00 - passed check; 0b10 - failed check
-int check_prohibit_move(int board_state[][BOARD_SIZE], int last_put[]) {
+// Check forbidden function: check if Black has performed forbidden moves
+// return `int`:
+//     000 - No forbidden moves;
+//     010 - Black has forbidden moves;
+int check_forbidden_move(int board_state[][BOARD_SIZE], int last_put[]) {
 
 }
 
 // Check full function: check if board is full
-// return `int`: 0b000 - not full; 0b100 - full
+// return `int`:
+//     0000 - not full;
+//     0100 - full
 int check_full_board(int board_state[][BOARD_SIZE], int last_put[]) {
 
 }
--- check.h
+++ check.h
@@ -2,5 +2,5 @@
 
 int check(int board_state[][BOARD_SIZE], int last_put[]);
 int check_win(int board_state[][BOARD_SIZE], int last_put[]);
-int check_prohibit_move(int board_state[][BOARD_SIZE], int last_put[]);
+int check_forbidden_move(int board_state[][BOARD_SIZE], int last_put[]);
 int check_full_board(int board_state[][BOARD_SIZE], int last_put[]);
