e389e5724b8fd3ef01048787940d5df488aa5206
Chen-Yuanmeng <2784841400@qq.com>
2024-12-27 08:45:14 +0800

    Add header for zobrist.c

--- zobrist.h
+++ zobrist.h
@@ -4,4 +4,8 @@
 #include "chessboard.h"
 #include "xorshift.h"
 
+void initializeZobrist();
+unsigned long long computeZobristHash(int board[BOARD_SIZE][BOARD_SIZE]);
+unsigned long long updateZobristHash(unsigned long long currentHash, int oldState, int newState, Position pos);
+
 #endif
